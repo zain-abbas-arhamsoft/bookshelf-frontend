ca556a96c41801ca773b09da909d3594f6d3183e
zainabbas <zainabbaskhakhi123@gmail.com>
2024-03-25 04:44:59 +0500

    remove console

--- Book-Shelf-Frontend/redux/store.js
+++ Book-Shelf-Frontend/redux/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '@/redux/reducer/root';
 import { persistStore } from 'redux-persist';
-import customMiddleware from '@/middleware/custom';
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 export const store = configureStore({
     reducer: rootReducer,
