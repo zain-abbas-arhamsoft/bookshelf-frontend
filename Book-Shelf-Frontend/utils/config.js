8b4be92ff8b3aff959fc0144d54ceeb3b394c8d0
zainabbas@123 <zainabbaskhakhi123@gmail.com>
2024-03-25 14:47:24 +0500

    change live url

--- Book-Shelf-Frontend/utils/config.js
+++ Book-Shelf-Frontend/utils/config.js
@@ -1,3 +1,3 @@
 const BASE_URL = `http://localhost:8080/`;
-const LIVE_URL = `https://bookshelf-brown.vercel.app`;
+const LIVE_URL = `https://bookshelf-brown.vercel.app/`;
 export { BASE_URL, LIVE_URL };
