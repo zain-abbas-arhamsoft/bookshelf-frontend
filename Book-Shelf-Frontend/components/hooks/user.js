84277721c2f72b2cb753a5bc6187ea08b42811c3
zainabbas@123 <zainabbaskhakhi123@gmail.com>
2024-03-25 14:22:22 +0500

    change from base to live url

--- Book-Shelf-Frontend/components/hooks/user.js
+++ Book-Shelf-Frontend/components/hooks/user.js
@@ -1,12 +1,12 @@
 import { useQuery, useMutation } from "react-query";
-import { BASE_URL } from "@/utils/config";
+import { LIVE_URL } from "@/utils/config";
 
 export function useGenresQuery(accessToken) {
     return useQuery("genres", async () => {
-        const response = await fetch(`${BASE_URL}v1/book/genre`, {
-            headers: {
-                Authorization: accessToken
-            }
+        const response = await fetch(`${LIVE_URL}v1/book/genre`, {
+          headers: {
+            Authorization: accessToken,
+          },
         });
 
         if (!response.ok) {
@@ -19,10 +19,10 @@ export function useGenresQuery(accessToken) {
 
 export function useGetBooksQuery(accessToken) {
     return useQuery("books", async () => {
-        const response = await fetch(`${BASE_URL}v1/book/books`, {
-            headers: {
-                Authorization: accessToken
-            }
+        const response = await fetch(`${LIVE_URL}v1/book/books`, {
+          headers: {
+            Authorization: accessToken,
+          },
         });
         if (response.status == 403)
             throw new Error(response.statusText)
@@ -34,62 +34,62 @@ export function useGetBooksQuery(accessToken) {
 }
 
 export function useUserRegisterMutation() {
-    return useMutation(newUser =>
-        fetch(`${BASE_URL}v1/user/register`, {
-            method: 'POST',
-            body: JSON.stringify(newUser),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        }).then(res => res.json())
-    )
+    return useMutation((newUser) =>
+      fetch(`${LIVE_URL}v1/user/register`, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((res) => res.json())
+    );
 }
 
 export function useUserLoginMutation() {
-    return useMutation(User =>
-        fetch(`${BASE_URL}v1/user/login`, {
-            method: 'POST',
-            body: JSON.stringify(User),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        }).then(res => res.json())
-    )
+    return useMutation((User) =>
+      fetch(`${LIVE_URL}v1/user/login`, {
+        method: "POST",
+        body: JSON.stringify(User),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((res) => res.json())
+    );
 }
 
 export function useCreateBookMutation(accessToken) {
     return useMutation((bookData) =>
-        fetch(`${BASE_URL}v1/book/create`, {
-            method: "POST",
-            headers: {
-                Authorization: accessToken
-            },
-            body: bookData,
-        }).then((res) => res.json())
+      fetch(`${LIVE_URL}v1/book/create`, {
+        method: "POST",
+        headers: {
+          Authorization: accessToken,
+        },
+        body: bookData,
+      }).then((res) => res.json())
     );
 }
 
 export function useBookStatusMutation(accessToken) {
     return useMutation((bookStatus) => {
-        fetch(`${BASE_URL}v1/book/update-status`, {
-            method: "PUT",
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                Authorization: accessToken,
-            },
-            body: JSON.stringify(bookStatus),
-        }).then((res) => res.json())
+        fetch(`${LIVE_URL}v1/book/update-status`, {
+          method: "PUT",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: accessToken,
+          },
+          body: JSON.stringify(bookStatus),
+        }).then((res) => res.json());
     });
 };
 
 export function useDeleteBookMutation(accessToken) {
     return useMutation(() => {
-        fetch(`${BASE_URL}v1/book/delete-books`, {
-            method: "DELETE",
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                Authorization: accessToken,
-            },
-        }).then((res) => res.json())
+        fetch(`${LIVE_URL}v1/book/delete-books`, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: accessToken,
+          },
+        }).then((res) => res.json());
     });
 };
--- Book-Shelf-Frontend/utils/config.js
+++ Book-Shelf-Frontend/utils/config.js
@@ -1,2 +1,3 @@
 const BASE_URL = `http://localhost:8080/`;
-export { BASE_URL };
+const LIVE_URL = `https://bookshelf-brown.vercel.app`;
+export { BASE_URL, LIVE_URL };
